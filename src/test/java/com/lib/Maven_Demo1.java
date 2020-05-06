b93e12c2c4e7591f960ab6a2a816e7d3cd3755e7
unknown <srinijg@gmail.com>
2020-05-06 03:50:56 -0400

    changes

--- src/test/java/com/lib/Maven_Demo1.java
+++ src/test/java/com/lib/Maven_Demo1.java
@@ -16,11 +16,12 @@ public class Maven_Demo1
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Public\\Downloads\\Eclipse\\geckodriver-v0.23.0-win64\\geckodriver.exe");
 		WebDriver Driver=new FirefoxDriver();
-		Driver.get("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=");
+		//Driver.get("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=");
+		Driver.get("https://freecrm.com/");
 		Driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		Driver.manage().window().maximize();
 		String title=Driver.getTitle();
-		String eTitle="Sign in or Register | eBay";
+		String eTitle="Free CRM #1 cloud software for any business large or small";
 		System.out.println("the title is "+title);
 		Assert.assertEquals(title, eTitle);
 		System.out.println("Git integration2");
